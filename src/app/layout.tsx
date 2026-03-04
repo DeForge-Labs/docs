911f99947ab72afc0acf3844385a437dd3286201
Shamba Chowdhury <38806897+ShambaC@users.noreply.github.com>
2026-03-05 00:43:19 +0530

    feat: add Simple Analytics script to layout for tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import { Lexend_Deca } from 'next/font/google';
+import Script from 'next/script';
 
 const lexend = Lexend_Deca({
   subsets: ['latin'],
@@ -9,9 +10,14 @@ const lexend = Lexend_Deca({
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={lexend.className} suppressHydrationWarning>
+      <script
+        async
+        src="https://scripts.simpleanalyticscdn.com/latest.js"
+      ></script>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
     </html>
   );
 }
